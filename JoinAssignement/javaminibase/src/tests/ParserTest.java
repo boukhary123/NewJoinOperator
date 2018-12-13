a6b1d55165aea537907464772bcce3842fd77884
kandakji <kondakji.oussama@gmail.com>
2018-12-13 13:00:16 +0100

    fixed bugs

--- JoinAssignement/javaminibase/src/tests/ParserTest.java
+++ JoinAssignement/javaminibase/src/tests/ParserTest.java
@@ -14,12 +14,46 @@ import catalog.*;
 import tests.*;
 
 
-public class ParserTest {
+public class ParserTest  implements GlobalConst {
 	
-	  public static void main(String argv[])
+	  public ParserTest()
 	  {
-		  File query_file = new File("../../query_2a.txt");
+		  String dbpath = "/tmp/"+System.getProperty("user.name")+".minibase.jointestdb"; 
+		    String logpath = "/tmp/"+System.getProperty("user.name")+".joinlog";
+
+		    String remove_cmd = "/bin/rm -rf ";
+		    String remove_logcmd = remove_cmd + logpath;
+		    String remove_dbcmd = remove_cmd + dbpath;
+		    String remove_joincmd = remove_cmd + dbpath;
+
+		    try {
+		      Runtime.getRuntime().exec(remove_logcmd);
+		      Runtime.getRuntime().exec(remove_dbcmd);
+		      Runtime.getRuntime().exec(remove_joincmd);
+		    }
+		    catch (IOException e) {
+		      System.err.println (""+e);
+		    }
+
+		   
+		    /*
+		    ExtendedSystemDefs extSysDef = 
+		      new ExtendedSystemDefs( "/tmp/minibase.jointestdb", "/tmp/joinlog",
+					      1000,500,200,"Clock");
+		    */
+
+		    SystemDefs sysdef = new SystemDefs( dbpath, 1000, NUMBUF, "Clock" );
+
+		  
+		  File query_file = new File("../../query_1a.txt");
 		  QueryParser q = new QueryParser(query_file);
 	  }
+	  
+	  public static void main(String argv[])
+	  {
+		  ParserTest test = new ParserTest();
+
+	  }
+
 
 }
